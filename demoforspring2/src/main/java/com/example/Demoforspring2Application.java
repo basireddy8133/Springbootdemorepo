93c0545cc34ae669a41fe5dfe7e5c6751fb5d96f
kallam <basavaiah.kallam>
2017-02-13 22:54:38 +0530

    commiting to our data

---
+++ demoforspring2/src/main/java/com/example/Demoforspring2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoforspring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoforspring2Application.class, args);
+		//hello basoi reddy
+	}
+}
